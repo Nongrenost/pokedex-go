3e1649a90493056b8a2a0caca30c43f26b2b68ee
Nongrenost <Nongrenost@gmail.com>
2025-10-10 22:55:18 +0300

    updated gitignore

---
+++ repl_test.go
@@ -0,0 +1,15 @@
+package main
+import(
+	"testing"
+)
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+	input    string
+	expected []string
+	}{
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+	}
+}
