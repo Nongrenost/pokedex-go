ebe61f2adebf573f2d4242344d32c0382065e049
Nongrenost <Nongrenost@gmail.com>
2025-10-14 19:39:53 +0300

    yea

--- repl_test.go
+++ repl_test.go
@@ -17,15 +17,9 @@ func TestCleanInput(t *testing.T) {
 
 	for _, c := range cases {
 		actual := cleanInput(c.input)
-		// Check the length of the actual slice against the expected slice
-		// if they don't match, use t.Errorf to print an error message
-		// and fail the test
 		for i := range actual {
 			word := actual[i]
 			expectedWord := c.expected[i]
-			// Check each word in the slice
-			// if they don't match, use t.Errorf to print an error message
-			// and fail the test
 		}
 	}
 }
