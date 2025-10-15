b0cd804dc965d52122fef16b5eb844450ac11fd5
Nongrenost <Nongrenost@gmail.com>
2025-10-15 20:13:01 +0300

    repl testing

---
+++ repl.go
@@ -0,0 +1,8 @@
+package main
+
+func cleanInput(text string) []string {
+    // split by whitespaces
+	//lowercase
+	//trim leading or trailing whitespace
+	return nil
+}
--- repl_test.go
+++ repl_test.go
@@ -13,13 +13,24 @@ func TestCleanInput(t *testing.T) {
 			input:    "  hello  world  ",
 			expected: []string{"hello", "world"},
 		},
+		//make a list of cases
+		//add test cases for them one by one
+		//write functionale to satisfy tests one by one
 	}
 
 	for _, c := range cases {
 		actual := cleanInput(c.input)
+		
+		if len(actual) != len(c.input) {
+			t.Errorf("resulting slice length != expected slice length")
+		}
+
 		for i := range actual {
 			word := actual[i]
 			expectedWord := c.expected[i]
+			if word != expectedWord {
+				t.Errorf("expected %s, got %s", expectedWord, word)
+			}
 		}
 	}
 }
