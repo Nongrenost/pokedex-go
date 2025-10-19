4c4bd60eb6491ad3044688be4683737a7f637ffd
Nongrenost <Nongrenost@gmail.com>
2025-10-19 18:18:12 +0300

    REPL

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
+
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for i := 0;; i++ {
+		fmt.Print("Pokedex > ")
+		var input string 
+
+		if scanner.Scan() {
+			input = scanner.Text()
+		}
+
+		firstWord := cleanInput(input)[0]
+		fmt.Printf("Your command was: %s\n", firstWord)
+	}
 }
 
